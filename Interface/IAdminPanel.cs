134bd5dfc3bea2431777975c01b2b9f9c261eab8
FaeyzSalaheen <faez.alabadi@gmail.com>
2025-06-25 02:48:57 +0300

    implement AdminPanal

---
+++ Interface/IAdminPanel.cs
@@ -0,0 +1,12 @@
+﻿using TechSummary.DTOs.AdminPanel;
+
+namespace TechSummary.Interface
+{
+    public interface IAdminPanel
+    {
+        Task<bool> AddCategoryAsync(string name);
+        Task<bool> UpdateCategoryAsync(int id, string newName);
+        Task<bool> DeleteCategoryAsync(int id);
+        Task<List<CategoryDto>> GetAllCategoriesAsync();
+    }
+}
