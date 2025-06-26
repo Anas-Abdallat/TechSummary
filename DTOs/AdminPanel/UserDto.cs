4c9f576191bb9bf66f5c92b7922ad5966ab3ece2
FaeyzSalaheen <faez.alabadi@gmail.com>
2025-06-27 01:50:34 +0300

    update AdminPanel

--- DTOs/AdminPanel/UserDto.cs
+++ DTOs/AdminPanel/UserDto.cs
@@ -2,5 +2,15 @@
 {
     public class UserDto
     {
+
+        public int Id { get; set; }
+
+        public string? FullName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public int? RoleId { get; set; }
+
+        public bool? IsActive { get; set; }
     }
 }
